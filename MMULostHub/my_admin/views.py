13b53080d188372698d96af876aaef4c9ed82a14
TzeYing007 <tzeying0802@gmail.com>
2026-04-18 04:51:31 +0800

    fixed view.py

--- MMULostHub/my_admin/views.py
+++ MMULostHub/my_admin/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def admin_feedback_view(request):
-    return render(request, 'my_admin/adminfeedback.html')
+    feedback_data = Feedback.object.select_related('user').all().order_by('-created_at')
+    return render(request, 'my_admin/adminfeedback.html', {'feedback': feedback_data})
