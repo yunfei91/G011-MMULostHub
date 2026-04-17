13c84ee962ffe5e2ca0c77adf1d747e330447684
TzeYing007 <tzeying0802@gmail.com>
2026-04-18 04:34:09 +0800

    improve urls

--- MMULostHub/report/services.py
+++ MMULostHub/report/services.py
@@ -1,11 +1,13 @@
 from .models import Feedback
 
-def create_feedback(comments, image=None):
+def create_feedback(comments, image=None, user=None):
 
     #a service function specifically responsible for saving feedback data to the database
     feedback = Feedback(
         comments=comments,
         image=image
+        user=user,
+        status='Panding'
     )
     feedback.save()
     return feedback
--- MMULostHub/report/views.py
+++ MMULostHub/report/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render, redirect
 from  .services import create_feedback
+from django.contrib.auth.decorators import login_required
 
-def feedback_view(request):
+@login_required
+def feedback_form_view(request):
     if request.method == "POST":
         comments = request.POST.get('comments')
         image = request.FILES.get('image-upload') 
 
         #call service function
-        create_feedback(comments=comments, image=image)
+        create_feedback(comments=comments, image=image, user=request.user)
 
-        return render(request, 'report/feedback.html', {'success': True})
+        return redirect('mainPage')
     
     return render(request, 'report/feedback.html')
