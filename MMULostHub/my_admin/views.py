d521bbfbe00324ee55b08549f454cf4667f8d5de
TzeYing007 <tzeying0802@gmail.com>
2026-04-18 04:58:09 +0800

    fixed typo

--- MMULostHub/my_admin/views.py
+++ MMULostHub/my_admin/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def admin_feedback_view(request):
-    feedback_data = Feedback.object.select_related('user').all().order_by('-created_at')
+    feedback_data = Feedback.objects.select_related('user').all().order_by('-created_at')
     return render(request, 'my_admin/adminfeedback.html', {'feedback': feedback_data})
