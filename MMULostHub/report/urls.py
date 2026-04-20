cfff9c82c385729f7a7d7aa54e393b041fe8c84f
TzeYing007 <tzeying0802@gmail.com>
2026-04-20 21:04:33 +0800

    From testing : update report app

--- MMULostHub/report/urls.py
+++ MMULostHub/report/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from .views import feedback_form_view
 
 urlpatterns = [
-    path('feedback/', views.feedback_view, name='feedback'),
+    path('feedback/', feedback_form_view, name='feedback_form'),
 ]
