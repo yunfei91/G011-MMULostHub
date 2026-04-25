9b4ca8e1507d8213af7c3d8437ba54c5ba8ee053
KHO YEE THENG <kho.yee.theng@student.mmu.edu.my>
2026-04-26 01:21:58 +0800

    Set username as email to avoid crash problem

--- MMULostHub/user/services.py
+++ MMULostHub/user/services.py
@@ -1,12 +1,16 @@
 from django.contrib.auth.models import User
 
-def create_user_account(username, email, password):
+def create_user_account(name, email, password):
+    
+    username=email
+
     if User.objects.filter(username=username).exists():
         username = username + email.split("@")[0]
         
     user = User.objects.create_user(
         username=username,
         email=email,
-        password=password
+        password=password,
+        first_name=name
     )
     return user
