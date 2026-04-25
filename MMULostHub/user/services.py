dffb3c6f574d438b5b565d32c109e02dd0390d0d
KHO YEE THENG <kho.yee.theng@student.mmu.edu.my>
2026-04-25 21:33:37 +0800

    Solved error when register

--- MMULostHub/user/services.py
+++ MMULostHub/user/services.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 
 def create_user_account(username, email, password):
-    if User.objects.filter(username=username).exits():
+    if User.objects.filter(username=username).exists():
         username = username + email.split("@")[0]
         
     user = User.objects.create_user(
