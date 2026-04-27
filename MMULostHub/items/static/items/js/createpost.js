9d6d29b952d9cc36b844431a0069c09a497b4204
yf-91 <tan.fee2007@gmail.com>
2026-04-27 12:59:56 +0800

    change branch

--- MMULostHub/items/static/items/js/createpost.js
+++ MMULostHub/items/static/items/js/createpost.js
@@ -11,4 +11,162 @@ document.addEventListener("DOMContentLoaded", function () {                 //  
         }
     });
 
+    const mapSmall = document.getElementById("smallMap");
+    const mapBig = document.getElementById("bigMap");
+
+    const modal = document.getElementById("map_modal");
+    const closeBtn = document.getElementById("close_map");
+
+    const bigMarker = document.getElementById("bigMap_marker");
+    const smallMarker = document.getElementById("smallMap_marker");
+
+    const locationSelect = document.getElementById("post_location");
+
+    const regions = [
+        {
+            code: "fci",
+            type: "rectangle",
+
+            x1: 782,
+            x2: 612,
+
+            y1: 1289,
+            y2: 1568,
+        },
+
+        {
+            code: "fom",
+            type: "polygon",
+
+            points: [
+                [841, 836],
+                [599, 1211],
+                [827, 1284],
+            ]
+        },
+    ];
+
+    // open map modal
+    mapSmall.addEventListener("click", function(){
+        modal.style.display = "block";
+    });
+
+    //close map modal
+    closeBtn.addEventListener("click", function(){
+        modal.style.display = "none";
+    });
+
+    //click big map
+    mapBig.addEventListener("click", function(event){
+        const rect = mapBig.getBoundingClientRect();
+
+        const x = event.clientX - rect.left;
+        const y = event.clientY - rect.top;
+
+        console.log("Clicked Position:", x, y);
+
+        //big map marker
+        bigMarker.style.display = "block";
+        bigMarker.style.left = x + "px";
+        bigMarker.style.top = y + "px";
+
+        //small map marker
+        const scaleX = mapSmall.clientWidth / mapBig.clientWidth;
+        const scaleY = mapSmall.clientHeight / mapBig.clientHeight;
+
+        smallMarker.style.display = "block"; 
+        smallMarker.style.left = (x * scaleX) + "px"; 
+        smallMarker.style.top = (y * scaleY) + "px";
+
+        //auto detect location
+        let foundLocation = false;
+        
+        for (let region of regions){
+
+            //rectangle
+            if (region.type === "rectangle"){
+                if(
+                    x >= region.x1 && 
+                    x <= region.x2 && 
+                    y >= region.y1 && 
+                    y <= region.y2
+                ){
+                    selectLocation(region.code);
+                    foundLocation = true;
+                    break;
+                }
+            }
+
+            //polygon
+            if (region.type === "polygon"){
+                
+                if(
+                    isPointInsidePolygon(
+                        x,
+                        y,
+                        region.points
+                    )
+                ){
+                    selectLocation(region.code);
+                    foundLocation = true;
+                    break;
+                }                
+            }
+        }
+
+        //no match location found in the area
+        if(!foundLocation){
+            alert("This area is not assigned to any MMU places. Please choose another area.");
+        }
+    });
+
+    //select dropdown
+    function selectLocation(locationCode){
+        
+        for (let option of locationSelect.options){
+            
+            if (option.value === locationCode){
+                option.selected = true;
+                break;
+            }
+        }
+    }
+
+    //check point is inside area or not
+    function isPointInsidePolygon(x,y,polygon){
+        
+        let inside = false;
+
+        for(
+            let i = 0, j = polygon.length - 1;
+            i < polygon.length;
+            j = i ++
+        ){
+            let xi = polygon[i][0]; 
+            let yi = polygon[i][1]; 
+
+            let xj = polygon[j][0]; 
+            let yj = polygon[j][1]; 
+            
+            let intersect = 
+                ( 
+                    (yi > y) !== (yj > y) 
+                ) 
+                && 
+                ( 
+                    x < 
+                    ( 
+                        ((xj - xi) * (y - yi)) / (yj - yi) 
+                    ) 
+                    + xi 
+                ); 
+            
+            if (intersect) { 
+                inside = !inside; 
+            }
+        }
+
+        return inside;
+    }
+
 });
