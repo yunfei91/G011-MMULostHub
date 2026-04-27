adf1acb2cf3b98740b08363983d1c84cc0996046
KHO YEE THENG <kho.yee.theng@student.mmu.edu.my>
2026-04-27 13:16:59 +0800

    Set name as unique

--- MMULostHub/user/models.py
+++ MMULostHub/user/models.py
@@ -5,7 +5,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     name = models.CharField(max_length=100, blank=True)
-    bio = models.TextField(blank=True, null=True)
+    bio = models.TextField(blank=True, unique=True)
     avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
 
     def __str__(self):
