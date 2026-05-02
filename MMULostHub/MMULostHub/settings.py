120c490faccdd2dc6a87fadaad2c948cff883737
KHO YEE THENG <kho.yee.theng@student.mmu.edu.my>
2026-05-02 11:52:19 +0800

    Added latest admin acc

--- MMULostHub/MMULostHub/settings.py
+++ MMULostHub/MMULostHub/settings.py
@@ -127,7 +127,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'kytmelody@gmail.com'
-EMAIL_HOST_PASSWORD = 'rinqbafunhoadjdi'
+EMAIL_HOST_USER = 'adminlosthub@gmail.com'
+EMAIL_HOST_PASSWORD = 'mauqafdeqnkwjkay'
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
