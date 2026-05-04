34e04deda276d63cc6d381e2f2fa30b759b2588d
TzeYing007 <tzeying0802@gmail.com>
2026-05-04 13:52:03 +0800

    feedbackform.html

--- MMULostHub/report/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Feedback
-
-@admin.register(Feedback)
-class FeedbackAdmin(admin.ModelAdmin):
-    list_display = ('id', 'comments', 'created_at')
