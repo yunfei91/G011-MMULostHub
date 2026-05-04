afc34b56af4f61e1ddc993e0b7e5c2b21bec74d4
TzeYing007 <tzeying0802@gmail.com>
2026-05-04 13:52:50 +0800

    models.py

--- MMULostHub/report/models.py
+++ MMULostHub/report/models.py
@@ -1,17 +1,15 @@
-from django.db import models
-from django.contrib.auth.models import User
+from django.contrib import admin
+from report.models import Feedback, Report
+from my_admin.email_utils import send_feedback_confirmation
 
-# Create your models here.
-class Feedback(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    comments = models.TextField()
-    image = models.ImageField(upload_to='feedback_images/', null=True, blank=True)
-    status = models.CharField(
-        max_length=20, 
-        choices=[('Pending', 'Pending'), ('Resolved', 'Resolved')],
-        default='Pending'
-    )
-    created_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return f"Feedback {self.id} - {self.created_at}"
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ('id', 'comments', 'status', 'created_at')
+
+
+class ReportAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'post', 'status', 'created_at')
+
+
+admin.site.register(Feedback, FeedbackAdmin)
+admin.site.register(Report, ReportAdmin)
