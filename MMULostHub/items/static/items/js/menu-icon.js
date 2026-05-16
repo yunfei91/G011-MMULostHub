107101f79db66000de7c39c6eae76c04cc3f9ea0
KHO YEE THENG <kho.yee.theng@student.mmu.edu.my>
2026-05-16 15:54:02 +0800

    Added effect of header shrink, menu icon transform effect and closing menu after scrolling the page

--- MMULostHub/items/static/items/js/menu-icon.js
+++ MMULostHub/items/static/items/js/menu-icon.js
@@ -15,3 +15,37 @@ function closeMenu(){
     document.getElementById("overlay").classList.remove("show");
     document.querySelector(".menu-icon").classList.remove("active");
 }
+
+let scrollTimer;
+const menu = document.getElementById("menu");
+const header = document.querySelector("header");
+
+let isShrink = false;
+
+window.addEventListener("scroll", () => {
+
+    const current = window.pageYOffset;
+
+    // When scrolling downward exceed 120, header will shrink
+    if(current > 120 && !isShrink){
+
+        header.classList.add("shrink");
+        isShrink = true;
+    }
+
+    // When scrolling upward until left 40, header back to original size
+    else if(current < 40 && isShrink){
+
+        header.classList.remove("shrink");
+        isShrink = false;
+    }
+
+    // menu auto close
+    if (menu.classList.contains("show-menu")) {
+        clearTimeout(scrollTimer);
+
+        scrollTimer = setTimeout(() => {
+            closeMenu();
+        }, 20);
+    }
+});
