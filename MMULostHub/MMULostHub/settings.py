cf3fd0a35184b9870a9e0d8a0c8d7250dfd418cc
KHO YEE THENG <kho.yee.theng@student.mmu.edu.my>
2026-05-29 12:14:45 +0800

    Fixed password bug

--- MMULostHub/MMULostHub/settings.py
+++ MMULostHub/MMULostHub/settings.py
@@ -93,9 +93,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
