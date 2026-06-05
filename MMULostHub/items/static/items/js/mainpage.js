48c004398e82a14e6fce89cbb79b7b1af4740b84
yf-91 <tan.fee2007@gmail.com>
2026-06-06 02:48:04 +0800

    fix delete popup no appear bug

--- MMULostHub/items/static/items/js/mainpage.js
+++ MMULostHub/items/static/items/js/mainpage.js
@@ -1,34 +1,30 @@
 /* ====================================== 
-        PLEASE LOGIN POPUP        
-    ====================================== */
-function pleaseLoginPopup() {
-    // when user not login and want to create post will change to user login page and show popup
-        showConfirmPopup(
-        "Login Required",
-        "Please login first.",
-        () => {
-
-            // redirect to login page
-            window.location.href = "/user/user-login/";
-        }
-    );
-}
-
-/* ====================================== 
         DELETE POST CONFIRMATION       
    ======================================= */
+let deleteForm = null;
+
 function confirmDelete(event, form) {
     event.preventDefault();
 
-    showConfirmPopup(
-        "CONFIRMATION",
-        "Are you sure you want to DELETE this post?",
-        () => {
-            form.submit();
-        }
-    );
+    deleteForm = form;
+
+    document.getElementById("deletePostPopup").style.display = "flex";
 }
 
+function confirmDeletePost() {
+
+    if (deleteForm) {
+        deleteForm.submit();
+    }
+}
+
+function closeDeletePopup() {
+    document.getElementById("deletePostPopup").style.display = "none";
+}
+
+/* ====================================== 
+            SEARCH & FILTER POST        
+   ======================================= */
 function clearSearch() {
     const url = new URL(window.location.href);
     url.searchParams.delete("q");
