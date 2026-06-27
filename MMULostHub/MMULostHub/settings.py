f35d89f1b33f17394ea13d1f893a6a458f32ebd2
yf-91 <tan.fee2007@gmail.com>
2026-06-28 01:38:32 +0800

    try again to use gmail smtp

--- MMULostHub/MMULostHub/settings.py
+++ MMULostHub/MMULostHub/settings.py
@@ -127,8 +127,14 @@ LOGOUT_REDIRECT_URL = 'beginning'
 LOGIN_URL = 'beginning'
 LOGIN_URL = '/user/user-login/'
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
-RESEND_API_KEY = os.getenv("RESEND_API_KEY")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 EMAIL_TIMEOUT = 20
 
