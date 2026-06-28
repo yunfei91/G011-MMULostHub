29a280c3b1aa002ca0f808adbb89764bd5b70d86
yf-91 <tan.fee2007@gmail.com>
2026-06-28 23:34:56 +0800

    try agian

--- MMULostHub/MMULostHub/settings.py
+++ MMULostHub/MMULostHub/settings.py
@@ -67,10 +67,12 @@ WSGI_APPLICATION = 'MMULostHub.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 DATABASES = {
     "default": dj_database_url.config(
-        default=os.getenv("DATABASE_URL"),
+        default=os.environ.get("DATABASE_URL"),
         conn_max_age=600,
+        ssl_require=True,
     )
 }
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
